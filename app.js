5be781a7b66d634b52395211f4adaa4bf0abb073
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-12-27 16:03:17 -0400

    Add CORS

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
 app.use(morgan('common'));
+app.use(cors());
 
 const books = require('./books.js');
 
